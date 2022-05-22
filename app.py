ebc514ab8cbca924e33c23a712a05fa629d3f99a
ravibatchala <batchalaraviteja@gmail.com>
2022-05-23 01:42:22 +0530

    canged requirements

--- app.py
+++ app.py
@@ -2,26 +2,29 @@ from flask import Flask, Response, render_template, request, redirect, url_for
 import cv2
 import numpy as np
 import mediapipe as mp
-from tensorflow import keras
+# from tensorflow import keras
+import keras
 import pandas as pd
 from mp_detection_fn import *
 
 app = Flask(__name__)
 cap = cv2.VideoCapture(0)
 
-mp_holistic = mp.solutions.holistic # Holistic model
-mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+mp_holistic = mp.solutions.holistic  # Holistic model
+mp_drawing = mp.solutions.drawing_utils  # Drawing utilities
 
-actions = np.array(['thank you', 'help', 'finish', 'please', 'see you later', 'sorry', 'no sign'])
+actions = np.array(['thank you', 'help', 'finish', 'please',
+                   'see you later', 'sorry', 'no sign'])
 
 model = keras.models.load_model(r'model5.h5')
+
 
 def frames():
     sequence = []
     sentence = []
     predictions = []
     threshold = 0.999
-    with mp_holistic.Holistic(min_detection_confidence=0.5,min_tracking_confidence=0.5) as holistic:
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while cap.isOpened():
             success, frame = cap.read()
             if not success:
@@ -30,45 +33,46 @@ def frames():
                 # Make detections
                 image, results = mediapipe_detection(frame, holistic)
                 print(results)
-        
+
                 # Draw landmarks
                 draw_landmarks(image, results)
-        
+
                 # 2. Prediction logic
                 keypoints = extract_keypoints(results)
                 sequence.append(keypoints)
                 sequence = sequence[-30:]
-        
+
                 if len(sequence) == 30:
                     res = model.predict(np.expand_dims(sequence, axis=0))[0]
                     print(actions[np.argmax(res)])
                     predictions.append(np.argmax(res))
-            
-            
-                #3. Viz logic
-                    if np.unique(predictions[-10:])[0]==np.argmax(res): 
-                        if res[np.argmax(res)] > threshold: 
-                    
-                            if len(sentence) > 0: 
+
+                # 3. Viz logic
+                    if np.unique(predictions[-10:])[0] == np.argmax(res):
+                        if res[np.argmax(res)] > threshold:
+
+                            if len(sentence) > 0:
                                 if actions[np.argmax(res)] != sentence[-1]:
                                     sentence.append(actions[np.argmax(res)])
                             else:
                                 sentence.append(actions[np.argmax(res)])
 
-                    if len(sentence) > 1: 
+                    if len(sentence) > 1:
                         sentence = sentence[-1:]
-            
+
                 cv2.rectangle(image, (0, 410), (769, 505), (8, 8, 8, 0.75), -1)
-                cv2.putText(image, ' '.join(sentence), (250,450), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+                cv2.putText(image, ' '.join(sentence), (250, 450),
+                            cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
                 ret, buffer = cv2.imencode('.jpg', image)
                 frame = buffer.tobytes()
                 yield (b'--frame\r\n'
                        b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/video_feed')
 def video_feed():
